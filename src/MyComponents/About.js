a3d9f59517de898fd952f6fb16794de3f6499096
Tanzeel Ur Rehman <khawajatanzeel.55221@gmail.com>
2022-08-16 01:02:57 +0500

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      This is an About MyComponents
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam tenetur,
+        minima quas tempore perspiciatis optio consequuntur quaerat in fugit
+        tempora dignissimos libero mollitia aliquid accusantium deleniti sed
+        neque possimus autem error praesentium perferendis. Voluptate.
+      </p>
+    </div>
+  );
+};
+
+export default About;
